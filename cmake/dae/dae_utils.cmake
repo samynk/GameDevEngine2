80fbdb6016a0ec8e7e659a9ac957af35ae6037f2
Koen Samyn <samyn.koen@gmail.com>
2023-02-03 10:38:35 +0100

    correct source directory

--- cmake/dae/dae_utils.cmake
+++ cmake/dae/dae_utils.cmake
@@ -1,9 +1,10 @@
 function(DefineLibraries)
-set( SDL2_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Libraries/SDL2-2.0.10")
-set( SDL2_IMAGE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Libraries/SDL2_image-2.0.5")
-set( SDL2_TTF_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Libraries/SDL2_ttf-2.0.15")
-set( SDL2_MIXER_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Libraries/SDL2_mixer-2.0.4")
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake/sdl2)
+message("Current root directory : ${CMAKE_CURRENT_SOURCE_DIR}")
+set( SDL2_PATH "${CMAKE_SOURCE_DIR}/Libraries/SDL2-2.0.10")
+set( SDL2_IMAGE_PATH "${CMAKE_SOURCE_DIR}/Libraries/SDL2_image-2.0.5")
+set( SDL2_TTF_PATH "${CMAKE_SOURCE_DIR}/Libraries/SDL2_ttf-2.0.15")
+set( SDL2_MIXER_PATH "${CMAKE_SOURCE_DIR}/Libraries/SDL2_mixer-2.0.4")
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/sdl2)
 # Add SDL2 library
 find_package(SDL2 REQUIRED)
 # Add SDL2_image library
